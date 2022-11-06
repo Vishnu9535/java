e066a0f95d9e3eb74012cf0ec07613831c6d53be
Vishnu9535 <vk9255888@gmail.com>
2022-11-06 00:19:34 -0400

    ll

--- data structures/src/linkedlist.java
+++ data structures/src/linkedlist.java
@@ -1,63 +1,33 @@
 // import java.util.*;
 // import java.io.*;
 public class linkedlist {
-    Node head;
-    static class Node{
+    Node  head;
+    class Node{
         int value;
-        Node next;
+        Node next; 
 
         Node(int d){
             value=d;
             next=null;
         }
     }
-    public void insertatend(int new_data){
-      Node new_node=new Node(new_data);
-      // System.out.println(head);
-      if(head==null){
-        head=new Node(new_data);
-      }
-      // System.out.println(head.value);
-      new_node.next=null;
-      // System.out.println(head.value);
-      Node last=head;
-      // System.out.println(last.next);
-      if(last.next !=null){
-        last=last.next;
-        // System.out.println(last.next.value);
-      }
-      last.next=new_node;
-
+    public void insert_begining(int data){
+      Node new_node=new Node(data);
+      new_node.next=head;
+      head=new_node;
     }
-       public static void main(String[] args){
-        
-        // LinkedList<Integer> a=new LinkedList<>();
-        // a.add(6);
-        // a.add(8);
-        // System.out.println(a);
-        linkedlist l1=new linkedlist();
-        // l1.head=new Node(1);
-        // //System.out.println(l1.head);
-        // Node second=new Node(2);
-        // Node third=new Node(3);
-        // l1.head.next=second;
-        // // System.out.println(l1.head.next.value);
-        // // l1.head.next=third;
-        // // System.out.println(l1.head.next.value);
-        // second.next=third;
-        
-        l1.insertatend(6);
-        l1.insertatend(9);
-        while(l1.head !=null)
-        {
-            System.out.println(l1.head.value+" ");
-            l1.head=l1.head.next;
-            
-        }
-
-        // System.out.println(l1.head.value);
-        // System.out.println(l1.head);
-        //system.out.println
-            //system.out.println()
+    public void print_thedetails(){
+    Node curr_node=head;
+    while(curr_node!=null){
+        System.out.println(curr_node.value);
+        curr_node=curr_node.next;
+    }
+    }
+     public static void main(String[] args){
+          linkedlist obj1=new linkedlist();
+          obj1.insert_begining(8);
+          obj1.insert_begining(6);
+          obj1.insert_begining(4);
+          obj1.print_thedetails();
     }
 }
---
+++ data structures/src/trash.java
@@ -0,0 +1,5 @@
+public class trash {
+    public static void main(String[] args) {
+        
+    }
+}
