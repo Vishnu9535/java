f557b08c3d0b99929df2ed79cfd49b8a3ce4bafb
Vishnu9535 <vk9255888@gmail.com>
2022-11-06 01:39:49 -0400

    ok

--- data structures/src/linkedlist.java
+++ data structures/src/linkedlist.java
@@ -1,5 +1,5 @@
 // import java.util.*;
-// import java.io.*;
+import java.io.*;
 public class linkedlist {
     Node  head;
     class Node{
@@ -16,6 +16,14 @@ public class linkedlist {
       new_node.next=head;
       head=new_node;
     }
+    public void insert_inend(int data ){
+        Node new_node=new Node(data);
+        Node curr_node=head;
+        while(curr_node.next!=null){
+            curr_node=curr_node.next;
+        }
+        curr_node.next=new_node;
+    }
     public void print_thedetails(){
     Node curr_node=head;
     while(curr_node!=null){
@@ -23,11 +31,31 @@ public class linkedlist {
         curr_node=curr_node.next;
     }
     }
+    public void delete_node(int key){
+        Node curr=head;
+        if(head.value==key){
+           head=curr.next;
+           curr.next=null;
+        }
+        else{
+          while(curr.next!=null){
+            if(curr.next.value==key){
+                curr.next=head;
+                curr.next=null;
+                break;
+            }
+            curr=curr.next;
+          }
+        }
+    }
      public static void main(String[] args){
           linkedlist obj1=new linkedlist();
           obj1.insert_begining(8);
           obj1.insert_begining(6);
           obj1.insert_begining(4);
+          obj1.insert_inend(5);
+          obj1.insert_inend(1);
+          obj1.delete_node(5);
           obj1.print_thedetails();
     }
 }
