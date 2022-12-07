bed10756f01068590887064d4817915c5c58429d
Vishnu9535 <vk9255888@gmail.com>
2022-12-07 09:41:50 +0530

    .....

--- data structures/src/ll.java
+++ data structures/src/ll.java
@@ -1,10 +1,10 @@
 public class ll {
-    Node head=null;
+    Node head;
     class Node{
         int data;
         Node next;        
         Node(int value){
-        data=value;
+        data=value; 
         next=null; 
         }
     }
@@ -18,6 +18,29 @@ public class ll {
         new_node.next=head; 
         head=new_node;
         }
+    }
+    void insert_at_the_end(int data){
+        if(head==null){
+            head=new Node(data);
+            head.next=null;
+        }
+        else{
+            Node curr=head;
+            while(curr.next!=null){
+                curr=curr.next;
+            }
+            curr.next=new Node(data);
+        }
+    }
+    void delete_first(){
+        head=head.next;
+    }
+    void delete_end(){
+        Node curr=head.next;
+        while(curr.next!=null){
+            curr=curr.next;
+        }
+        curr.next=null;
     }
     void print(){
         if(head==null){
@@ -36,6 +59,9 @@ public class ll {
         ll x1=new ll();
         x1.insert_at_first(80);
         x1.insert_at_first(0);
+        x1.insert_at_the_end(90);
+        // x1.delete_first();
+        x1.delete_end();
         x1.print();
     }
 }
