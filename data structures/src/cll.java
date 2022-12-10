0af01b8b0b6eb80c43af7ff3a7a87b0e558473ea
Vishnu9535 <vk9255888@gmail.com>
2022-12-10 19:16:14 +0530

    ..

--- data structures/src/cll.java
+++ data structures/src/cll.java
@@ -13,6 +13,26 @@ public class cll {
         if(head==null){
             head=n1;
             tail=n1;
+            return;
         }
+        tail.next=n1;
+        n1.next=head;
+        tail=n1;        
+    }
+    void display(){
+        Node curr=head;
+        while(curr!=tail){
+            System.out.println(curr.data);
+            curr=curr.next;
+        }
+        System.out.println(curr.next.data);
+        }
+    public static void main(String[] args) {
+        cll x1=new cll();
+        x1.insert_end(55);
+        x1.insert_end(85);
+        x1.insert_end(66);
+        x1.insert_end(77);
+        x1.display();
     }
 }
