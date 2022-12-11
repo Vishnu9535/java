dedc35b56b639f5908a0fa76b871af12ecf6d308
Vishnu9535 <vk9255888@gmail.com>
2022-12-11 13:08:49 +0530

    ...

--- java learn/src/oops.java
+++ java learn/src/oops.java
@@ -1,3 +1,16 @@
-public class oops {
-    
+interface car{
+    void car_speed(int x);
+    void gear(int y);
 }
+class oops implements car{
+    public void car_speed(int x){
+     System.out.println("the car speed is "+x);
+    }
+    public void gear(int y){
+        System.out.println("gear of the car is "+y);
+    }
+
+    public static void main(String[] args) {
+            
+        }
+    }
