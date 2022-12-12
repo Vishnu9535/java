04cb9a906830c59f52aae60f5916111d89629744
Vishnu9535 <vk9255888@gmail.com>
2022-12-12 23:14:21 +0530

    ...

--- data structures/src/stackqueue.java
+++ data structures/src/stackqueue.java
@@ -4,5 +4,27 @@ public class stackqueue {
     class Node{
         int data;
         Node next;
+        Node(int data){
+            this.data=data;
+        }
+
+    }
+    void en_queue(int data){
+        Node n1=new Node(data);
+        if(head==null){
+            head=n1;
+            tail=n1;
+            return;
+        }
+        tail.next=n1;
+        tail=n1;
+    }
+    void  de_queue(){
+        if(head==null){
+            System.out.println("nothing to pop");
+            return;
+        }
+        System.out.println("dequed item is "+head.data);
+        head=head.next;
     }
 }
