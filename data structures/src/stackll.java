3d6a44da50e8e886e7d561a988578076f0a1971d
Vishnu9535 <vk9255888@gmail.com>
2022-12-12 22:46:00 +0530

    .....

--- data structures/src/stackll.java
+++ data structures/src/stackll.java
@@ -6,5 +6,28 @@ public class stackll {
     Node(int data){
         this.data=data;
     }
+    void push(int data){
+        Node n1=new Node(data);    
+    if(head==null){
+         head=n1;
+         head.next=null;
+         return;
+        }
+    n1.next=head;
+    head=n1;
     }
+    int pop(){
+        if(head==null){
+            System.out.println("nothing to pop");
+            return -1;
+        }
+        else{
+        int x= head.data;
+        if(head.next!=null){
+        head=head.next;
+        }
+        return x;
+    }
+    }
+}
 }
