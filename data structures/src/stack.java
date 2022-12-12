067646f2119fb6901ffe93dfeba1538b179fe7fb
Vishnu9535 <vk9255888@gmail.com>
2022-12-12 08:50:18 +0530

    ...

--- data structures/src/stack.java
+++ data structures/src/stack.java
@@ -1,7 +1,31 @@
 import java.util.*;
 class stack{
+    int top;
+    int a[];
+    int capacity;
+    stack(int size){
+        capacity=size;
+        a=new int[size];
+        top=-1;
+    }
+    void push(int value){
+        if(top==capacity){
+            System.out.println("stack is full");
+            return;
+        }
+          top++;
+          a[top]=value;
+    }
+    int  pop(){
+        return a[top];
+    }
+    void full_check(){
+          if(top==capacity){
+            System.out.println("stack is full");
+          }
+    }
     public static void main(String[] args) {
-        Stack<Integer> x1=new Stack<>();
+        // Stack<Integer> x1=new Stack<>();
         
     }
 }
