cd7599811216363e6548afba02feb9460ef258f1
Vishnu9535 <vk9255888@gmail.com>
2022-12-12 18:30:19 +0530

    ....

--- data structures/src/sparx.java
+++ data structures/src/sparx.java
@@ -14,13 +14,24 @@ public class sparx {
              mat[i][j]=sc.nextInt();
             }
         }
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+            //  mat[i][j]=sc.nextInt();
+System.out.println(mat[i][j]);
+        }
+        }
+        
         int unique=0;
+        System.out.println("sparx mat is ");
         for(int i=0;i<row;i++){
         for(int j=0;j<col;j++){
               if(mat[i][j]!=0){
                 unique++;
+                System.out.println(i+" "+j+"" +" "+mat[i][j]);
+                
               }
         }
         }
+        // System.out.println(unique);
     }
 }
