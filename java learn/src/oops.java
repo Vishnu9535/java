541fe8c7503a348d3f0b353234727715aa8ea226
Vishnu9535 <vk9255888@gmail.com>
2022-12-12 08:37:02 +0530

    ....

--- java learn/src/oops.java
+++ java learn/src/oops.java
@@ -9,10 +9,22 @@ class oops implements car{
     public void gear(int y){
         System.out.println("gear of the car is "+y);
     }
+    void a(int m){
+        System.out.println("hi");
+    }
+    void a(String z){
+        System.out.println("hello "+z);
+    }
+    void a(int n,int k){
+        System.out.println(n+k);
+    }
 
     public static void main(String[] args) {
             oops x1=new oops();
             x1.car_speed(50);
             x1.gear(5);
+            x1.a("this is my car");
+            x1.a(55);
+            x1.a(77,65);
         }
     }
