c8a4eb2fe1066ca8be14c87a14be23b40790bd98
Vishnu9535 <vk9255888@gmail.com>
2022-12-12 19:25:49 +0530

    .....

---
+++ data structures/src/queue.java
@@ -0,0 +1,12 @@
+public class queue {
+    int size;
+    int[] q;
+    int front,rear;
+    queue(int size){
+        this.size=size;
+        q=new int[size];
+    }
+    public static void main(String[] args) {
+        
+    }
+}
