0911055b43e691f90529cc5737734d2f71ca2594
Vishnu9535 <vk9255888@gmail.com>
2022-12-16 18:30:01 +0530

    ....

--- data structures/src/inpostfix.java
+++ data structures/src/inpostfix.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 class stack{
     int maxsize=60;
-    int[] s=new int[maxsize];
+    char[] s=new char[maxsize];
     int top=-1;
     boolean isempty(){
         if(top==-1){
@@ -21,7 +21,7 @@ class stack{
             return false;
         }
     }
-    void push(int data){
+    void push(char data){
         if(isfull()){
             System.out.println("its full");
             return;
@@ -29,41 +29,71 @@ class stack{
         top++;
         s[top]=data;
     }
-    int  pop(){
-        if(isempty()){
-            System.out.println("nothng to pop");
-            return -1;
-        }
-        else{
-            int x= s[top];
+    char  pop(){
+     
+      
+            char x= (char) s[top];
             top--;
             return x;
         }
-    }
-    int peek(){
-        if(isempty()){
-            System.out.println("nothng to peek");
-            return -1;
-        }
-        else{
-            return s[top];
-        }
+    char peek(){        
+            return (char)s[top];
+        
     }
 
 } 
 public class inpostfix {
-      String topostfix(String a){
-        for(int i=0;i<a.length();i++){
-          
+   public static int prec(char x){
+        if(x=='+'||x=='-'){
+            return 1;
         }
-      }
+        else if(x=='*'||x=='%'){
+            return 2;
+        }
+        else{
+            return 0;
+        }
+    }
+    static String to_postfix(String infix){
+        stack operator=new stack();
+        char symbol;
+        String postfix="";
+        for(int i=0;i<infix.length();i++){
+          symbol=infix.charAt(i);
+          if(Character.isSpaceChar(i))
+             continue;
+          if(Character.isLetter(symbol)){
+            postfix=postfix+symbol;
+          }
+          else if(symbol=='('){
+            operator.push(symbol);
+          }
+          else if(symbol==')'){
+            while(operator.peek()!='('){
+                postfix=postfix+operator.pop();
+            }
+            operator.pop();
+        }
+            else{
+                while(!operator.isempty()&&operator.peek()!='('&& prec(symbol)>=operator.peek()){
+                   postfix=postfix+operator.pop();
+                }
+                operator.push(symbol);
+            }
+          }
+          while(!operator.isempty()){
+            postfix=postfix+operator.pop();
+          }
+          return postfix;
+        }
 
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        String a=sc.next();
-        // System.out.println(a);
-        
 
+        Scanner sc=new Scanner(System.in);
+        String infix=sc.nextLine();
+        System.out.println(infix);
+        String postfix=to_postfix(infix);
+        System.out.println("postfix string is "+postfix);
         
     }
 }
