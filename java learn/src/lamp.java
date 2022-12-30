e939c1b0ad01e3552164029ad4f4cd39f6bc1174
Vishnu9535 <vk9255888@gmail.com>
2022-12-30 07:34:22 +0530

    ..

--- java learn/src/lamp.java
+++ java learn/src/lamp.java
@@ -1,3 +1,29 @@
-public class lamp {
-    
+class lamp {
+   public  boolean lampon=false;
+    void changelamp(){
+        if(lampon){
+            lampon=false;
+        }
+        else{
+            lampon=true;
+        }
+    }
+    boolean return_status(){
+         return lampon;
+    }
+    void check_status(){
+        if(return_status()){
+            System.out.println("its on");
+            return;
+        }
+        System.out.println("its off");
+    } 
+    public static void main(String[] args) {
+        lamp x1=new lamp();
+        x1.check_status();
+        x1.changelamp();
+        x1.check_status();
+        x1.changelamp();
+        x1.check_status();
+    }
 }
