029746f49243973bd61ad6d0c300d1eff7a8b941
Vishnu9535 <97020373+Vishnu9535@users.noreply.github.com>
2023-02-23 07:41:55 +0000

    Pattern questions

--- data structures/src/pattern.java
+++ data structures/src/pattern.java
@@ -1,5 +1,7 @@
-class Main {
-  public static void main(String[] args) {    char a='*';
+class pattern
+{
+  public static void main (String args[]){
+    char a='*';
      for(int i=0;i<5;i++){
      for(int j=5;j>i;j--){
      System.out.print(a);
