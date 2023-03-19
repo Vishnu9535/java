f337e4e07a9a214159795048390fed1ad22ed4aa
Vishnu9535 <vk9255888@gmail.com>
2023-03-19 09:52:16 +0530

    .....

--- datastructures/src/gcd.java
+++ datastructures/src/gcd.java
@@ -1,13 +1,62 @@
+import java.util.*;
+import java.math.*;
 public class gcd {
     public static void main(String[] args) {
       
         int m=12;
         int n=8;
+
+        gcd a=new gcd();
+        // a.gcd2(m, n);
+        a.ersthonesis(7);
         while(n!=0){
             int r=m%n;
             m=n;
             n=r;
+            // System.out.println(m+" "+n);
         }
-        System.out.println(m);
+        // System.out.println(m);
+    }
+    public void gcd1(int a,int b){
+          while(a!=b){
+            if(a>b){
+                a=a-b;
+            }
+            else if(a<b){
+              b=b-a;
+            }            
+          }
+          System.out.println(a);
+    }
+    public void gcd2(int a, int b)
+    {
+      int r=Math.min(a,b);
+      while(r!=0){
+       if(a%r==0 && b%r==0){
+          break;
+       }
+        r--;
+      }
+      System.out.println(r);
+    }
+    public void ersthonesis(int a){
+      int[] arr=new int[a+1];
+         for(int i=2;i<=a;i++){
+          arr[i]=i;
+         }
+         for(int p=2;p<Math.sqrt(a);p++){
+          if(arr[p]!=0){
+              int x=p*p;
+              while(x<a){
+                arr[x]=0;
+                x=x+p; 
+              }
+          }
+         }
+         for(int i=2;i<arr.length;i++){
+          if(arr[i]!=0){
+            System.out.println(arr[i]);
+          }
+         }
     }
 }
--- datastructures/src/tempCodeRunnerFile.java
+++ datastructures/src/tempCodeRunnerFile.java
@@ -1,3 +1,2 @@
-        //   obj1.insert_inend(5);
-        //   obj1.insert_inend(1);
-        //   obj1.delete_node(5);
+
+            if(a>b){
