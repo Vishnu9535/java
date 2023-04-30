ecba0f8a4fe57b05bd40037817bbe44e0f428ba4
Vishnu9535 <vk9255888@gmail.com>
2023-04-30 23:30:09 +0530

    ...

--- datastructures/src/binary.java
+++ datastructures/src/binary.java
@@ -1,9 +1,10 @@
 public class binary {
     public static void main(String[] args) {
         binary o1=new binary();
-        int[] arr={1,2,3,4,5,6,7,8,9};
-        int target=7;
-        int result=o1.binarysearch(arr, target);
+        int[] arr={1,2,3,4,7,8};
+        int target=5;
+        int result=o1.cieling(arr, target);
+        // int result=o1.order_egnostic(arr, target);
         System.out.println(result);
     }
     int binarysearch(int[] arr,int target){
@@ -12,14 +13,15 @@ public class binary {
         int index=0;
         int comp=0;
         while(start<= end){
-        index=(start+end)/2;
+        index=start+(end-start)/2;
+        //start/2 + end/
         if(arr[index] == target){
             comp++;
             return index;
         }
         else if(arr[index]>target){
             comp++;
-            end=index-1;
+            end=index-1; 
         }
         else if(arr[index]<target){
             comp++;
@@ -29,4 +31,46 @@ public class binary {
         System.out.println(comp);
         return -1;
         }
+        int order_egnostic(int[] arr,int target){   
+              boolean check=arr[0] < arr[arr.length-1];
+              int start=0;
+              int end=arr.length-1;
+              while(start <= end){
+              int index=start + (end - start) / 2;
+              if(arr[index]==target){
+                return index;
+              }
+              if(check){
+                if(target < arr[index]){
+                    end=index-1;
+                }
+                else{
+                    start=index+1;
+                }
+              }
+              else{
+                if(target < arr[index])
+                {
+                    start=index+1;        
+                }
+                else
+                {
+                    end=index-1;
+                }
+            }
+        }
+            
+              return -1;
+}
+    int cieling(int[] arr , int target){
+        int start=0;
+        while(arr[start] < target){
+            start++;
+        }
+        return arr[start];
+    }
+    int cieling2(int[] arr , int target){
+        int start=0;
+
+    }
 }
--- leatcode/problems/src/problem80.java
+++ leatcode/problems/src/problem80.java
@@ -1,16 +1,25 @@
-public class problem80 {
+import java.util.HashMap;
+
+public class problem80 
+{
     public int removeDuplicates(int[] nums) {
         int k=0;
-
+        HashMap<Integer ,Integer> v=new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            int m=nums[i];
-            int n=nums[i];
-         if(nums[i]==m){
-        
-         }
+            if(v.containsKey(nums[i])){
+                nums[i]++;
+            }
+            else {
+                v.put(nums[i], 1);
         }
-        return k;
     }
+     for(int i = 0; i < nums.length; i++){
+        if(v.get(i) >= 2){
+              nums[i]=v.get()
+        }
+    }
+         return k;
+}
     public static void main(String[] args) {
         int[] arr={1,1,1,2,2,3};
         problem80 obj1=new problem80();
