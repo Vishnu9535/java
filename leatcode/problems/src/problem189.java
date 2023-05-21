913cb9227c0104aea83a9447ca8a46a22ff4516f
Vishnu9535 <vk9255888@gmail.com>
2023-05-21 19:49:51 +0530

    ....

--- leatcode/problems/src/problem189.java
+++ leatcode/problems/src/problem189.java
@@ -2,6 +2,9 @@ import java.util.Arrays;
 // function rotate2 is right ans
 class problem189{
     public void rotate(int[] nums, int k) {
+        if(nums.length<k){
+            k=k-nums.length;
+        }
         int i=0;
         while(i!=k){
             nums=turn(nums);
@@ -22,18 +25,71 @@ class problem189{
 
     }
     public void rotate2(int[] nums, int k) {
-        int n=nums.length-1-k;
-        
+    //   int m=k;
+    //   for(int i=0;i<k;i++){
+    //     int x=nums[nums.length-m];
+    //     nums[nums.length-m]=nums[i];
+    //     nums[i]=x;
+    //     m=m-1;
+    //   }
+    //   System.out.println(Arrays.toString(nums));
+    //   m=k;
+    //   int z=nums.length-1;
+    //   for(int i=nums.length-m;i<nums.length-1;i++){
+    //     int x=nums[i];
+    //     nums[i]=nums[z];
+    //     nums[z]=x;
+    //     z--;
+    //     m--;
+    //   }
+    System.out.println(Arrays.toString(nums));
+  if(nums.length<k){
+    k=k-nums.length;
+  }
+  if(nums.length>k){
+    int m=k;
         for(int i=0;i<k;i++){
-            int v=nums[n];
-            nums[n]=nums.length-1;
+            int x=nums[i];
+            nums[i]=nums[nums.length-m];
+            nums[nums.length-m]=x;
+            m--;
+        }
+        if(nums.length-k>1){
+        System.out.println(Arrays.toString(nums));
+        m=k;
+        int j=nums.length-1;
+        for(int i=0;i<k/2;i++){
+            int x=nums[nums.length-m];
+            nums[nums.length-m]=nums[j];
+            nums[j]=x;
+            j--;
+            m--;
+        }
+        System.out.println(Arrays.toString(nums));
+        int v=nums.length-k-1;
+        for(int i=0;i<(nums.length-2*k)/2;i++){
+            int x=nums[i+k];
+            nums[i+k]=nums[v];
+            nums[v]=x;
+            v--;
+        }
+        System.out.println(Arrays.toString(nums));
+        int t=nums.length-1;
+        for(int i=0;i<((nums.length-k)/2);i++){
+            int x=nums[i+k];
+            nums[i+k]=nums[t];
+            nums[t]=x;
+            t--;
         }
     }
+        System.out.println(Arrays.toString(nums));
+    }
+}
 
     public static void main(String[] args) {
-        int[] nums={1,2,3,4,5,6,7};
-        int k=3;
+        int[] nums={1,2,3,4,5,6};
+        int k=4;
         problem189 obj=new problem189();
-        obj.rotate(nums, k);
+        obj.rotate2(nums, k);
     }
 }
---
+++ leatcode/problems/src/problem21.java
@@ -0,0 +1,15 @@
+public class problem21 {
+    Node head;
+    class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+            next=null;
+        }
+}
+    public static void main(String[] args) {
+        
+    }
+}
+
