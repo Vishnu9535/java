ec4ac6cb9fc5eae6777f23a5319895f763ab1812
Vishnu9535 <vk9255888@gmail.com>
2023-06-17 10:51:07 +0530

    ...

--- leatcode/problems/src/problem35.java
+++ leatcode/problems/src/problem35.java
@@ -1,17 +1,27 @@
 public class problem35 {
     public int searchInsert(int[] nums, int target) {
-        int< left = 0;
+        int left = 0;
         int right = nums.length-1;
+        int mid=0;
         while(left <= right){
-            
+            mid=left + (right-left)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[mid] > target){
+                right=mid-1;
+            }
+            if( nums[mid] < target){
+                left = mid+1;
+            }
         }
-        
-        return -1;
+        return left;
     }
     public static void main(String[] args) {
      int[] nums={1,3,5,6};
-     int target=7;
+     int target=4;
      problem35 obj1=new problem35();
-
+     int x=obj1.searchInsert(nums, target);
+     System.out.println(x);
     }
 }
