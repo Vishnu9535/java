0eccde0b726a642223c5732a8dcd1812fbbcb82f
Vishnu9535 <vk9255888@gmail.com>
2023-07-10 23:32:20 +0530

    ...

--- datastructures/src/App.java
+++ datastructures/src/App.java
@@ -1,5 +1,16 @@
+import java.util.*;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) 
+    { double tc,famt,disc;  
+    Scanner scnr=new Scanner(System.in);
+    System.out.println("Enter the price of the colth");
+    tc =scnr.nextDouble();
+    if(tc<2000){
+        disc=(tc*5)/100;
+        tc=tc-disc;
     }
+    System.err.println(tc);
+}
 }
