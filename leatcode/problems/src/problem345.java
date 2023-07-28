0c377944dba560e4251871f27c47227ab99a2d0b
Vishnu9535 <vk9255888@gmail.com>
2023-07-28 21:54:49 +0530

    is

--- leatcode/problems/src/problem345.java
+++ leatcode/problems/src/problem345.java
@@ -1,13 +1,13 @@
 public class problem345 {
     public static void main(String[] args) {
         problem345 obj1 = new problem345();
-        System.out.println(obj1.reverseVowels("hello"));
+        System.out.println(obj1.reverseVowels2("hello"));
     }    
     public String reverseVowels(String s) {
         char[] s1=s.toCharArray();
         int index=-1;
         for(int i=0; i< s1.length;i++){
-            if (s1[i] == 'a' || s1[i] == 'e' || s1[i] == 'i' || s1[i] == 'o' || s1[i] == 'u' )
+            if (s1[i] == 'a' || s1[i] == 'e' || s1[i] == 'i' || s1[i] == 'o' || s1[i] == 'u'|| s1[i] == 'A' || s1[i] == 'E' || s1[i] == 'I' || s1[i] == 'O' || s1[i] == 'U' )
             { 
              if(index != -1){
                 char temp=s1[i];
@@ -19,6 +19,31 @@ public class problem345 {
         }
         s=String.copyValueOf(s1);
         return s;
+    }
+    public String reverseVowels2(String s){
+      char[] arr1=s.toCharArray();
+      int start=0;
+      int end = s.length()-1;
+      while(start < end){
+        if(not_vowel(arr1[start])){
+            start++;
+        }
+        if(not_vowel(arr1[end])){
+            end--;
+        }
+        else{
+            char temp = arr1[start];
+            arr1[start] = arr1[end];
+            arr1[end] = temp;
+        }
+      }  
+    }
+    public static boolean not_vowel(char c){
+        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I'|| c == 'O' || c == 'U')
+        {
+            return false;
+        }
+        return true;
     }
 
 }
