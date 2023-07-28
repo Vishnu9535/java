f949ab93be66cb3d865142402e3cc4e7655751b5
Vishnu9535 <vk9255888@gmail.com>
2023-07-28 20:34:19 +0530

    ...

--- leatcode/problems/src/problem345.java
+++ leatcode/problems/src/problem345.java
@@ -1,21 +1,26 @@
 public class problem345 {
     public static void main(String[] args) {
         problem345 obj1 = new problem345();
+        System.out.println(obj1.reverseVowels("hello"));
     }    
     public String reverseVowels(String s) {
         char[] s1=s.toCharArray();
-        char x='y';
-        int index;
+        int index=-1;
         String s2="";
         for(int i=0; i< s1.length;i++){
             if (s1[i] == 'a' || s1[i] == 'e' || s1[i] == 'i' || s1[i] == 'o' || s1[i] == 'u' )
-            {
-             if(x != 'y'){
+            { 
+             if(index != -1){
                 char temp=s1[i];
-                s1[i]=temp;
+                System.out.println(s1[i]);  
+                s1[i]=s1[index];
+                System.out.println(s1[i]);
+                s1[index]=temp;
+                System.out.println(s1[index]);
+
              }
-             x=s1[i];
              index=i;
+             
             }
             s2=s2+s1[i];
         }
