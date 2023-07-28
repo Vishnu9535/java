aff88868276e80c276f0131f2e59bc5580ad569c
Vishnu9535 <vk9255888@gmail.com>
2023-07-28 21:05:19 +0530

    udsvb

--- leatcode/problems/src/problem345.java
+++ leatcode/problems/src/problem345.java
@@ -6,26 +6,19 @@ public class problem345 {
     public String reverseVowels(String s) {
         char[] s1=s.toCharArray();
         int index=-1;
-        String s2="";
         for(int i=0; i< s1.length;i++){
             if (s1[i] == 'a' || s1[i] == 'e' || s1[i] == 'i' || s1[i] == 'o' || s1[i] == 'u' )
             { 
              if(index != -1){
                 char temp=s1[i];
-                System.out.println(s1[i]);  
                 s1[i]=s1[index];
-                System.out.println(s1[i]);
                 s1[index]=temp;
-                System.out.println(s1[index]);
-
              }
              index=i;
-             
             }
-            s2=s2+s1[i];
         }
-        return s2;
-
+        s=String.copyValueOf(s1);
+        return s;
     }
 
 }
