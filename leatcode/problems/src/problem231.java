46a12247ddc774a7c6daa93bd96303431bc35f5a
Vishnu9535 <vk9255888@gmail.com>
2023-08-04 22:10:28 +0530

    problem0

--- leatcode/problems/src/problem231.java
+++ leatcode/problems/src/problem231.java
@@ -1,23 +1,31 @@
+import java.math.*;
+import java.util.*;
 class problem231{
     public static void main(String[] args) {
-        int x=100;
-        isPowerOfTwo(x);
+        int x=9;
+        // System.out.println(1.5 % 1);
+        System.out.println(isPowerOfTwo(5));
     }
      public static boolean isPowerOfTwo(int n) {
-        if(n == 1){
+        // double sq=n/2;
+        // sq= square_root(n, sq);
+        // if(sq % 2 == 0){
+        //     return true;
+        // }
+        // return false;
+        double x= Math.log(n)/Math.log(2);
+            System.out.println(x);
+        double threshold=0.000001;
+        if(Math.abs( x % 1) <= threshold) 
             return true;
-        }
-        double sq=n/2;
-        sq= square_root(n, sq);
-        System.out.println(sq);
-        return true;
+        return false;
     }
-    public static double square_root(int n, double sq){
-            double t= sq;
-            sq= (t + (n/t))/2;
-            if(t - sq == 0){
-                return sq;
-            }
-            return square_root(n, sq);
-    }
+    // public static double square_root(int n, double sq){
+    //         double t= sq;
+    //         sq= (t + (n/t))/2;
+    //         if(t - sq == 0){
+    //             return sq;
+    //         }
+    //         return square_root(n, sq);
+    // }
 }
