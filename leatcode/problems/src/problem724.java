5f24fb2855b38b9ca2b028c3969f6677b6597fb3
Vishnu9535 <vk9255888@gmail.com>
2023-08-24 00:04:29 +0530

    leet code

--- leatcode/problems/src/problem724.java
+++ leatcode/problems/src/problem724.java
@@ -7,13 +7,16 @@ public class problem724 {
         }
         int left_sum=0;
         for(int i = 0;i < nums.length ; i++){
-            if(left_sum == total - nums[i] - left_sum)
+            if(left_sum == total - nums[i] - left_sum){
+                return i;
+            }
+            left_sum += left_sum + nums[i];
         }
-
+        return -1;  
     }
     public static void main(String[] args) {
         int[] nums = {1,7,3,6,5,6};
         problem724 obj1 = new problem724();
-        obj1.pivotIndex(nums);
+        System.out.println(obj1.pivotIndex(nums));
     }
 }
