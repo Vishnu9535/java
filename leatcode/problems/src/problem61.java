8b855300f282444838b351cd1292109dbf388549
Vishnu9535 <vk9255888@gmail.com>
2023-08-25 18:10:04 +0530

    qustion

--- leatcode/problems/src/problem61.java
+++ leatcode/problems/src/problem61.java
@@ -37,22 +37,34 @@ class problem61{
         }
     }
     public ListNode rotateRight(ListNode head, int k) {
+        if(head == null || head.next == null || k ==0){
+            return head;
+        }
         ListNode temp = head ;
-        int length = 0;
-        while (temp != null){
+        int length = 1;
+        while (temp.next != null){
             temp = temp.next;
             length = length+1;
         }
+        while(k >= length){
+            k = k - length;
+        }
+        if(k == 0){
+            return head;
+        }
+        temp.next =head;
         // System.out.println(length-k);
-        temp = head;
-        for(int i =0; i< length-k;i++){
-            temp = temp.next;
+        ListNode p = head;
+        for(int i =0; i< length-k-1;i++){
+            p = p.next;
         }
-        System.out.println(temp.val);
-        ListNode n1 = temp ;
-        while(n1.next != null){
-            n1 = n1.next;
-        }
+        head = p.next ;
+        p.next=null;
+        // ListNode tem= head;
+        //  while(tem != null){
+        //      System.out.println(tem.val);
+        //      tem = tem.next;
+        //  }
         return head;
     }
     public static void main(String[] args) {
@@ -63,6 +75,6 @@ class problem61{
         obj.insert_begining(4);
         obj.insert_begining(5);
         // obj.print();
-        obj.rotateRight(head,2);
+        obj.rotateRight(head,3);
     }
 }
