40af5e68cc381464b860716b9c56519d4415fccb
Vishnu9535 <vk9255888@gmail.com>
2023-11-19 12:15:38 +0530

    ji

--- leatcode/problems/src/problem2438.java
+++ leatcode/problems/src/problem2438.java
@@ -1,6 +1,8 @@
+import java.util.Arrays;
+
 public class problem2438 {
-    public int[] productQueries(int n, int[][] queries) {
-        int[] answers = new int[queries.length];
+    public int[] productQueries(int n) {
+        int[] answers = new int[10];
         int[] binary = new int[32];
         int id = 0;
         while(n > 0){
@@ -8,9 +10,16 @@ public class problem2438 {
             id++;
             n=n/2;
         }
+        int[] powers = new int[id];
+        for(int i =0 ; i < id; i++){
+            powers[i] = (int) Math.pow(2,i)*binary[id-1];
+        }
+        System.out.println(Arrays.toString(powers));
         return answers;
     }
     public static void main(String[] args) {
-        
+
+        problem2438 obj1 = new problem2438();
+        obj1.productQueries(2);
     }
 }
