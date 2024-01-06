3c9951b002763e82783d626f4fbe882a811de0a5
Vishnu9535 <2110291@reva.edu.in>
2024-01-06 17:29:56 +0530

    b

--- leatcode/problems/src/problem2807.java
+++ leatcode/problems/src/problem2807.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class problem2807 {
-    ListNode head;
+    static ListNode head;
     class ListNode{
         int value;
         ListNode next;
@@ -19,24 +19,51 @@ public class problem2807 {
             ListNode curr = head;
 
             while (curr.next != null) {
-                
+                curr = curr.next;
             }
+            curr.next= new ListNode(data);
 
         }    
         
     }
+    void print(){
+        ListNode curr = head;
+        while (curr!=null) {
+            System.out.println(curr.value);
+            curr = curr.next;
+        }
+    }
     public ListNode insertGreatestCommonDivisors(ListNode head) {
         problem867 x = new problem867();
         ListNode result = head;
-        return result;
+        while (result.next != null){
+            int a = result.value;
+            int b = result.next.value;
+            int c = find_gcd(a, b);
+            ListNode temp=result.next;
+            result.next = new ListNode(c);
+            result = result.next;
+            result.next = temp;
+            result = result.next;
+        }
+        return head;
+    }
+    public static int find_gcd(int a , int b){
+        if (b == 0) {
+            return a;
+        } 
+        else {
+            return find_gcd(b, a % b);
+        }
     }
     public static void main(String[] args) {
         problem2807 obj1 = new problem2807();
         LinkedList<Integer> x1 = new LinkedList<>();
-        obj1.insert_inend(18);
-        obj1.insert_inend(6);
-        obj1.insert_inend(10);
-        obj1.insert_inend(30);
-        obj1.print_thedetails();
+        obj1.insert_end(18);
+        obj1.insert_end(6);
+        obj1.insert_end(10);
+        obj1.insert_end(3);
+        obj1.insertGreatestCommonDivisors(head);
+        obj1.print();
     }
 }
