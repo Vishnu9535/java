29814e08d880151790525989f6bffca19c61f47f
Vishnu9535 <2110291@reva.edu.in>
2024-02-18 23:38:46 +0530

    jbds

---
+++ datastructures/src/permutation.java
@@ -0,0 +1,17 @@
+public class permutation {
+    
+    public static void print_combination(String s,String f){
+            if(s.isEmpty()){
+                System.out.println(f);
+                return;
+            }
+            for(int i = 0 ; i <= f.length() ;i++){
+                String start = s.substring(i)
+            }
+
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
