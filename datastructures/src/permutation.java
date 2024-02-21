fc8efa57b82d46de71dcf25e6a6320832e040f6c
Vishnu9535 <2110291@reva.edu.in>
2024-02-21 23:42:49 +0530

    commit

--- datastructures/src/permutation.java
+++ datastructures/src/permutation.java
@@ -1,17 +1,48 @@
-public class permutation {
-    
-    public static void print_combination(String s,String f){
-            if(s.isEmpty()){
-                System.out.println(f);
-                return;
-            }
-            for(int i = 0 ; i <= f.length() ;i++){
-                String start = s.substring(i)
-            }
+import java.lang.reflect.Array;
+import java.util.*;
 
+public class permutation {
+
+    public static void print_combination(String s, String f, List<String> result) {
+        if (s.isEmpty()) {
+            System.out.println(f);
+            result.add(f);
+            return;
+        }
+        char ch = s.charAt(0);
+        for (int i = 0; i <= f.length(); i++) {
+            String start = f.substring(0, i);
+            // System.out.println(start);
+            String end = f.substring(i, f.length());
+            // System.out.println(end);
+            print_combination(s.substring(1), start + ch + end, result);
+        }
     }
-    
+
+    public static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(nums, new ArrayList<>(), result);
+        return result;
+    }
+
+    private static  void  backtrack(int[] nums, List<Integer> tempList, List<List<Integer>> result) {
+        if (tempList.size() == nums.length) {
+        result.add(new ArrayList<>(tempList));
+        return;
+  }
+        for (int i = 0; i < nums.length; i++) {
+            if (tempList.contains(nums[i]))
+                continue;
+            tempList.add(nums[i]);
+            backtrack(nums, tempList, result);
+            tempList.remove(tempList.size() - 1);
+  }
+}
+
     public static void main(String[] args) {
-        
+        ArrayList<String> result = new ArrayList<String>();
+        // print_combination("abc", "", result);
+        // System.out.println(result);
+        System.out.println(permute(new int[] { 1, 2, 3 }));
     }
 }
