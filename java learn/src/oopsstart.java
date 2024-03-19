23c3adfdaae8583201a77dc128bb63552a5f44b3
Vishnu9535 <2110291@reva.edu.in>
2024-03-19 15:05:43 +0530

    hvjhv

---
+++ java learn/src/oopsstart.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+
+class oopsstart{
+
+    class Student{
+        int[] rollno = new int[5];
+        String[] name = new String[5];
+        int[] marks = new int[5];
+    }
+    public static void main(String[] args) {
+     Student[] s = new Student[5];
+
+    }
+}
